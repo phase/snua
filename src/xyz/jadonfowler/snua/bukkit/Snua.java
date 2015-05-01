a08c64e16596f7eacc7407a2e783b09ed5ee83b0
phase <ph4sesaber@gmail.com>
2015-05-01 07:55:08 -0700

    Start Bukkit implementation

---
+++ src/xyz/jadonfowler/snua/bukkit/Snua.java
@@ -0,0 +1,15 @@
+package xyz.jadfowler.snua;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Snua extends JavaPlugin {
+
+  public void onEnable(){
+    
+  }
+
+  pubilc void onDisable(){
+    
+  }
+
+}
