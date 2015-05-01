220fc4e4a522441355c0e2cea249c4555af035e0
phase <ph4sesaber@gmail.com>
2015-05-01 07:59:39 -0700

    Add in Addressing Modes

--- src/xyz/jadonfowler/snua/nes/dom/AddressingMode.java
+++ src/xyz/jadonfowler/snua/nes/dom/AddressingMode.java
@@ -7,8 +7,19 @@ import java.util.HashMap;
  * Addressing Modes for the NES
  */
 public enum AddressingMode {
-
-  ;
+  IMPLICIT("Implied"),
+  ACCUMULATOR("Accumulator"),
+  IMMEDIATE("Immediate"),
+  ZERO_PAGE("Zero Page"),
+  ZERO_PAGE_X("Zero Page,X"),
+  ZERO_PAGE_Y("Zero Page,Y"),
+  RELATIVE("Relative"),
+  ABSOLUTE("Absolute"),
+  ABSOLUTE_X("Absolute,X"),
+  ABSOLUTE_Y("Absolute,Y"),
+  INDIRECT("Indirect"),
+  INDIRECT_X("(Indirect,X)"),
+  INDIRECT_Y("(Indirect),Y");
 
   private static HashMap<String, AddressingMode> modeMap = new HashMap<String, AddressingMode>;
 
